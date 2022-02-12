9c286ec3ed31cc7cda8cf5ee3f1b3d1f16ecc1f9
Meng Hang <chocolate.menghang@gmail.com>
2022-02-12 17:31:27 +0800

    SPI_HOST_MAX does not exist.
    Signed-off-by: Meng Hang <chocolate.menghang@gmail.com>

--- lvgl_helpers.c
+++ lvgl_helpers.c
@@ -154,7 +154,11 @@ bool lvgl_spi_driver_init(int host,
     int dma_channel,
     int quadwp_pin, int quadhd_pin)
 {
-    assert((0 <= host) && (SPI_HOST_MAX > host));
+#if (SOC_SPI_PERIPH_NUM == 2)
+    assert(host < SPI1_HOST && host > SPI2_HOST);
+#elif (SOC_SPI_PERIPH_NUM == 3)
+    assert(host < SPI1_HOST && host > SPI3_HOST);
+#endif
     const char *spi_names[] = {
         "SPI1_HOST", "SPI2_HOST", "SPI3_HOST"
     };
